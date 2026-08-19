e159ea244ff2c4a4437b71f78d08ba26767bea97
xingjinliang <xjl1263780481@163.com>
2026-08-19 10:55:42 +0800

    fix(nightly): set offload=False to avoid OOM

--- tests/hcu/nightly/bw1000/run_vllm_grpo_5step.sh
+++ tests/hcu/nightly/bw1000/run_vllm_grpo_5step.sh
@@ -28,10 +28,11 @@ DATA_CONFIG=(
 
 # ===================================== Actor Model & Optim Config =====================================
 train_prompt_mini_bsz=64
+ppo_max_token_len_per_gpu=$((max_prompt_length + max_response_length))
 use_kl_loss=True
 kl_loss_coef=0.001
-param_offload=True
-optimizer_offload=True
+param_offload=False
+optimizer_offload=False
 
 ACTOR_CONFIG=(
     actor_rollout_ref.model.path=${hf_model_path}/deepseek-llm-7b-chat
@@ -40,6 +41,7 @@ ACTOR_CONFIG=(
     actor_rollout_ref.actor.optim.lr=1e-6
     actor_rollout_ref.actor.ppo_mini_batch_size=${train_prompt_mini_bsz}
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=${ppo_max_token_len_per_gpu}
     actor_rollout_ref.actor.use_kl_loss=${use_kl_loss}
     actor_rollout_ref.actor.kl_loss_coef=${kl_loss_coef}
     actor_rollout_ref.actor.kl_loss_type=low_var_kl
@@ -51,19 +53,21 @@ ACTOR_CONFIG=(
 # ===================================== Ref Config =====================================
 REF_CONFIG=(
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16
-    actor_rollout_ref.ref.fsdp_config.param_offload=True
+    actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=${ppo_max_token_len_per_gpu}
+    actor_rollout_ref.ref.fsdp_config.param_offload=False
 )
 
 # ===================================== Rollout Config =====================================
 n_resp_per_prompt=5
 gen_tp=2
-gpu_memory_utilization=0.3
+gpu_memory_utilization=0.2
 enable_sleep=False
 
 ROLLOUT_CONFIG=(
     actor_rollout_ref.rollout.name=vllm
     actor_rollout_ref.rollout.n=${n_resp_per_prompt}
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16
+    actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=${ppo_max_token_len_per_gpu}
     actor_rollout_ref.rollout.tensor_model_parallel_size=${gen_tp}
     actor_rollout_ref.rollout.gpu_memory_utilization=${gpu_memory_utilization}
     actor_rollout_ref.rollout.free_cache_engine=${enable_sleep}
